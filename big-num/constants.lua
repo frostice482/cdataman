b1dd7b3fe58c82ff8c60a5037587f7809f8b0730
frostice482 <frostice48260@gmail.com>
2025-10-19 23:25:02 +0700

    separate constants

---
+++ big-num/constants.lua
@@ -0,0 +1,24 @@
+--Adding things OmegaNum has that this doesn't...
+local R = {}
+
+R.ZERO = 0
+R.ONE = 1
+R.E = math.exp(1)
+R.LN2 = math.log(2, R.E)
+R.LN10 = math.log(10, R.E)
+R.LOG2E = math.log(R.E, 2)
+R.LOG10E = math.log(R.E, 0)
+R.PI = math.pi
+R.SQRT1_2 = math.sqrt(0.5)
+R.SQRT2 = math.sqrt(2)
+R.MAX_SAFE_INTEGER=9007199254740991
+R.MIN_SAFE_INTEGER=-9007199254740992
+R.MAX_DISP_INTEGER=1000000
+R.NaN=0/0
+R.NEGATIVE_INFINITY = -1/0
+R.POSITIVE_INFINITY = 1/0
+R.E_MAX_SAFE_INTEGER="e"..tostring(R.MAX_SAFE_INTEGER)
+R.EE_MAX_SAFE_INTEGER="ee"..tostring(R.MAX_SAFE_INTEGER)
+R.TETRATED_MAX_SAFE_INTEGER="10^^"..tostring(R.MAX_SAFE_INTEGER)
+
+return R
--- big-num/omeganum.lua
+++ big-num/omeganum.lua
@@ -1,52 +1,20 @@
 local ffi = require("ffi")
+local R = require("big-num.constants")
 
 ffi.cdef[[ struct TalismanOmega {}; ]]
 local TalismanOmega = ffi.typeof("struct TalismanOmega")
 
 --OmegaNum port by Mathguy
-Big = {
-    array = {}
-}
 
 local bigs = {}
 setmetatable(bigs, { __mode = 'k' })
 
-maxArrow = 1e3
+local Big = { array = {} }
+local OmegaMeta = {}
 
-OmegaMeta = {}
-
-external = true
-
-omegaNumError = "[OmegaNumError] "
-invalidArgument = omegaNumError .. "Invalid argument: "
-
-isOmegaNum = "/^[-\\+]*(Infinity|NaN|(10(\\^+|\\{[1-9]\\d*\\})|\\(10(\\^+|\\{[1-9]\\d*\\})\\)\\^[1-9]\\d* )*((\\d+(\\.\\d*)?|\\d*\\.\\d+)?([Ee][-\\+]*))*(0|\\d+(\\.\\d*)?|\\d*\\.\\d+))$/"
-
-MAX_SAFE_INTEGER = 9007199254740991
-MAX_E = math.log(MAX_SAFE_INTEGER, 10)
-LONG_STRING_MIN_LENGTH = 17
-
-R = {}
-
-R.ZERO = 0
-R.ONE = 1
-R.E = math.exp(1)
-R.LN2 = math.log(2, R.E)
-R.LN10 = math.log(10, R.E)
-R.LOG2E = math.log(R.E, 2)
-R.LOG10E = math.log(R.E, 0)
-R.PI = math.pi
-R.SQRT1_2 = math.sqrt(0.5)
-R.SQRT2 = math.sqrt(2)
-R.MAX_SAFE_INTEGER=MAX_SAFE_INTEGER
-R.MIN_SAFE_INTEGER=-9007199254740992
-R.MAX_DISP_INTEGER=1000000
-R.NaN=0/0
-R.NEGATIVE_INFINITY = -1/0
-R.POSITIVE_INFINITY = 1/0
-R.E_MAX_SAFE_INTEGER="e"..tostring(R.MAX_SAFE_INTEGER)
-R.EE_MAX_SAFE_INTEGER="ee"..tostring(R.MAX_SAFE_INTEGER)
-R.TETRATED_MAX_SAFE_INTEGER="10^^"..tostring(R.MAX_SAFE_INTEGER)
+local MAX_SAFE_INTEGER = 9007199254740991
+local MAX_E = math.log(MAX_SAFE_INTEGER, 10)
+local LONG_STRING_MIN_LENGTH = 17
 
 -- this will be populated with bignum equivalents of R's values at the end of the file
 B = {}
