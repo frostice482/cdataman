0cdd3cb914cbaa73300dd4a3289e3499814c8f56
Jevonnissocoolman <jevonnsivels2@gmail.com>
2025-10-23 10:11:14 -0700

    increase version

--- steamodded_metadata.lua
+++ steamodded_metadata.lua
@@ -4,7 +4,7 @@
 --- MOD_AUTHOR: [MathIsFun_, Mathguy24, jenwalter666, cg-223, lord.ruby]
 --- MOD_DESCRIPTION: A mod that increases Balatro's score limit and skips scoring animations.
 --- PREFIX: talisman
---- VERSION: 2.5
+--- VERSION: 2.6
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
