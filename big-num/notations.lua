fbf2c96da669823691c56bdc157aea9406b47aef
frostice482 <frostice48260@gmail.com>
2025-11-19 10:23:31 +0700

    fix

--- big-num/notations.lua
+++ big-num/notations.lua
@@ -1,7 +1,5 @@
 local Notations = {
     ArrayNotation = require("big-num.notations.arraynotation"),
-    AnteNotation = require("big-num.notations.antenotation"),
-    LetterNotation = require("big-num.notations.letternotation"),
     Balatro = require("big-num.notations.Balatro"),
 }
 return Notations
